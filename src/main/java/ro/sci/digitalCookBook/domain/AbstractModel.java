c30a970525022372329e0aed13c153577535e354
Andrei <andreibu.ancora@gmail.com>
2019-11-01 23:41:26 +0200

    Exceptions and AbstractModel class

---
+++ src/main/java/ro/sci/digitalCookBook/domain/AbstractModel.java
@@ -0,0 +1,11 @@
+package ro.sci.digitalCookBook.domain;
+
+public abstract class AbstractModel{
+
+    private int id;
+
+    public long getId(){ return id;}
+
+    public void setId(int id) {this.id = id;}
+
+}
