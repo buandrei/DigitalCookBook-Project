9c7f7f34545b7985e86aab398613ca0da2495811
Andrei <andreibu.ancora@gmail.com>
2019-11-11 20:51:08 +0200

    diverse changes to index and recipe service

--- src/main/java/ro/sci/digitalCookBook/service/RecipeService.java
+++ src/main/java/ro/sci/digitalCookBook/service/RecipeService.java
@@ -37,6 +37,8 @@ public class RecipeService {
         return dao.searchForRecipeByIngredients(ingredients, moreIngredients);
     }
 
+
+
     public Collection<Recipe> getAllWherePromotionNotNull() {
 
         return dao.getAllWherePromotionNotNull();
